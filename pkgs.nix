1d11c7f00d1c5c7cf6ebf4be72a76c73effcb996
Sorin Iclanzan <sorin@iclanzan.com>
2023-04-14 12:17:03 -0400

    Use same nixpkgs as main repo

--- pkgs.nix
+++ pkgs.nix
@@ -1,9 +1,10 @@
 let
-  sha256 = "sha256:1fhv0lfj7khfr0fvwbpay3vq3v7br86qq01yyl0qxls8nsq08y0c";
-  rev = "872fceeed60ae6b7766cc0a4cd5bf5901b9098ec";
+  sha256 = "sha256:1lbf24nlx01gckq2cij9fxc6jv0lrviwliaghrwspfgyb2ph4cn5";
+  rev = "ac718d02867a84b42522a0ece52d841188208f2c";
 in
 builtins.trace "(Using pinned Nixpkgs at ${rev})"
-import (fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-  inherit sha256;
-})
+  import
+  (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+    inherit sha256;
+  })
